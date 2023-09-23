b206629d3d95b6b3abad1f030503a26609572de7
Raphael Matos <raphaelmsgoncalves@gmail.com>
2023-09-22 23:41:12 -0300

    Adicionei os botoes de música de fundo, com eles também já criei o arquivo .js para torná-los funcionais, no arquivo já criei as funções dos respectivos botoes da musica.

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const musicaDeFundoON = () => {
+    const musica = document.getElementById("soundtrack")
+    musica.play()
+}
+
+const musicaDeFundoOFF = () => {
+    const musica = document.getElementById("soundtrack")
+    musica.pause()
+}
